0490095076126726601d87706da2288f7ab4014e
Mike Downes <mikedownes.dev@gmail.com>
2023-10-10 12:05:00 -0400

    exercise 8

---
+++ ex8/ex8.rb
@@ -0,0 +1,17 @@
+# You can use the #{my_var} syntax to insert variables into strings.
+# You can also use the %{} syntax to do the same thing. But it's particularly 
+# useful when you want to apply the same format to multiple values.
+
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+    first: "I had this thing.",
+    second: "That you could type up right.",
+    third: "But it didn't sing.",
+    fourth: "So I said goodnight."
+}
